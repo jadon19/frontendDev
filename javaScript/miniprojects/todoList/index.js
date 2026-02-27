6aa1fd801fba7a0c9fd71db6b3935e0679515117
tanishka <tanishkajadon19@gmail.com>
2026-02-27 16:17:20 +0530

    todo- create task css and js

---
+++ javaScript/miniprojects/todoList/index.js
@@ -0,0 +1,10 @@
+// create an object of
+let tasks ={
+    "heading1":
+    {
+        "description":"",
+        "due-date":"",
+        "tag":"",
+        "status":"pending/completed/lapsed"
+    }
+};
--- javaScript/miniprojects/todoList/pages/js pages/create-task.js
+++ javaScript/miniprojects/todoList/pages/js pages/create-task.js
@@ -2,21 +2,43 @@ const createTask = document.querySelector('.js-open-task');
 const createTaskForm = document.querySelector(".js-create-task");
 console.log(createTask);
 createTask.addEventListener("click" ,()=>{
-    
-    // after user clicks '+' ->fetch the task creation form
-
     fetch("pages/create-task.html")
     .then(res=>res.text())
     .then(html=>{
         createTaskForm.innerHTML =html;
+
+
         const closeTask = document.querySelector(".js-close-icon");
         closeTask.addEventListener("click", ()=>{
             createTaskForm.innerHTML=``;
         })
+        let heading = document.querySelector('.js-heading');
+        let description = document.querySelector('.js-description');
+        let date = document.querySelector('.js-date-input');
+        let tagOptions = document.querySelectorAll('.tag-btn');
+        
+        let selectedTag;
+        tagOptions.forEach(btn => {
+            btn.addEventListener("click", () => {
+                if(btn.classList.contains("active")){
+                    btn.classList.remove("active");
+                }
+                else{
+                    tagOptions.forEach(b => b.classList.remove("active"));
+                    btn.classList.add("active");
+                }
+                selectedTag = btn;
+                console.log(selectedTag);
+            });
+        });
+        
     })
     .catch(err=>console.log("some error in loading the file",err));
 
     
 })
+function addTask(){
+    // to be called to add a task
+}
 
 
